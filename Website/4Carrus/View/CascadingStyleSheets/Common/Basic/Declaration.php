d4704768ebed34aaba08d6e1fe87c8ca7d3b8969
Judah Holanda Correia Lima <jh_esse@hotmail.com>
2014-06-19 22:19:23 -0300

    update

--- Website/4Carrus/View/CascadingStyleSheets/Common/Basic/Declaration.php
+++ Website/4Carrus/View/CascadingStyleSheets/Common/Basic/Declaration.php
@@ -1,30 +1,31 @@
 <?php
 
-    class Declaration {//TODO: terminar de testar
+class Declaration {//TODO: terminar de testar
 
-       private $stringProperty;
-       private $stringValue;
+    private $stringProperty;
+    private $stringValue;
 
-       private function __construct($stringProperty, $stringValue) {
-          $this->stringProperty = $stringProperty;
-          $this->stringValue = $stringValue;
-       }
-
-       //Constructor
-       //   instantiation: 
-       //      $declaration = Declaration::DeclarationWithStringPropertyWithStringValue($stringProperty, $stringValue);
-       public static function DeclarationWithStringPropertyWithStringValue($stringProperty, $stringValue) {
-          $instance = new self($stringProperty, ":".$stringValue);
-          return $instance;
-       }
-
-       public function __toString() {
-          return $this->stringDeclaration();
-       }
-
-       public function stringDeclaration() {
-          return $this->stringProperty . $this->stringValue . ";";
-       }
+    private function __construct($stringProperty, $stringValue) {
+        $this->stringProperty = $stringProperty;
+        $this->stringValue = $stringValue;
     }
+
+    //Constructor
+    //   instantiation: 
+    //      $declaration = Declaration::DeclarationWithStringPropertyWithStringValue($stringProperty, $stringValue);
+    public static function DeclarationWithStringPropertyWithStringValue($stringProperty, $stringValue) {
+        $instance = new self($stringProperty, $stringValue);
+        return $instance;
+    }
+
+    public function __toString() {
+        return $this->stringDeclaration();
+    }
+
+    public function stringDeclaration() {
+        return $this->stringProperty . ":" . $this->stringValue . ";";
+    }
+
+}
 
 ?>
