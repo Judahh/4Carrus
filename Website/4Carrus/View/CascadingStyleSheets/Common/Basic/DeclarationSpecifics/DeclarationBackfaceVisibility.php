5ce5766019cfdd7f4f105ed1b4341c4a94054789
Judah Holanda Correia Lima <jh_esse@hotmail.com>
2014-06-21 14:39:08 -0300

    update

---
+++ Website/4Carrus/View/CascadingStyleSheets/Common/Basic/DeclarationSpecifics/DeclarationBackfaceVisibility.php
@@ -0,0 +1,42 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of DeclarationColor
+ *
+ * @author Judah
+ */
+    class DeclarationBackfaceVisibility extends Declaration {
+
+        //Constructor
+        //   instantiation: 
+        //      $declaration = DeclarationAlignSelf::DeclarationAlignSelfWithStringValue($stringValue);
+        public static function DeclarationBackfaceVisibilityWithStringValue($stringValue) {
+            $instance = new parent("align-self", $stringValue); //arrumar
+            return $instance;
+        }
+        
+        public static function stringValueVisible() {
+            return "visible";
+        }
+        
+        public static function stringValueHidden() {
+            return "hidden";
+        }
+        
+        public static function stringValueInitial() {
+            return "initial";
+        }
+        
+        public static function stringValueInherit() {
+            return "inherit";
+        }
+        
+    }
+
+?>
