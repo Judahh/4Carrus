d0b0b4539b97ca78b898a096119a4c21fecea39f
Judah Holanda Correia Lima <jh_esse@hotmail.com>
2014-06-28 16:19:11 -0300

    first full Judah's part fix

--- Website/4Carrus/View/CascadingStyleSheets/Common/Basic/DeclarationSpecifics/DeclarationBackfaceVisibility.php
+++ Website/4Carrus/View/CascadingStyleSheets/Common/Basic/DeclarationSpecifics/DeclarationBackfaceVisibility.php
@@ -11,23 +11,33 @@
  *
  * @author Judah
  */
-    class DeclarationBackfaceVisibility extends Declaration {
+class DeclarationBackfaceVisibility extends Declaration {
 
-        //Constructor
-        //   instantiation: 
-        //      $declaration = DeclarationAlignSelf::DeclarationAlignSelfWithStringValue($stringValue);
-        public static function DeclarationBackfaceVisibilityWithStringValue($stringValue) {
-            $instance = new parent("align-self", $stringValue); //arrumar
-            return $instance;
-        }
-        
-        public static function stringValueVisible() {
-            return "visible";
-        }
-        
-        public static function stringValueHidden() {
-            return "hidden";
-        }
+    //Constructor
+    //   instantiation: 
+    //      $declaration = DeclarationAlignSelf::DeclarationAlignSelfWithStringValue($stringValue);
+    public static function DeclarationBackfaceVisibilityWithStringValue($stringValue) {
+        $instance = new parent("backface-visibility", $stringValue); //arrumar
+        return $instance;
     }
+
+    public function stringDeclaration() {
+        $stringFullDeclaration = "";
+        $arrayStringBrowserPrefix = BrowserDiffer::getInstance()->getArrayStringBrowserPrefix();
+        foreach ($arrayStringBrowserPrefix as &$stringBrowserPrefix) {
+            $stringFullDeclaration = $stringFullDeclaration . $stringBrowserPrefix . $this->stringProperty . ":" . $this->stringValue . ";";
+        }
+        return $stringFullDeclaration;
+    }
+
+    public static function stringValueVisible() {
+        return "visible";
+    }
+
+    public static function stringValueHidden() {
+        return "hidden";
+    }
+
+}
 
 ?>
