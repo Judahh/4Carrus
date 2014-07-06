5d25668ea3bcb5d5e71ffbac74cef5cf8121d149
Tarciso Torres <tarcisoftorres@gmail.com>
2014-07-06 16:21:24 -0300

    fix and update

--- Website/4Carrus/View/CascadingStyleSheets/Common/Basic/DeclarationSpecifics/DeclarationTabSize.php
+++ Website/4Carrus/View/CascadingStyleSheets/Common/Basic/DeclarationSpecifics/DeclarationTabSize.php
@@ -6,15 +6,44 @@
  * @copyright (c) year, Tarciso Torres Blue Lion
  */
 
+class DeclarationTabSize extends Declaration {
 //Constructor
 //   instantiation: 
 //      $declaration = DeclarationTabSize::DeclarationTabSizeWithStringValue($stringValue);
-class DeclarationTabSize extends Declaration {
     public static function DeclarationTabSizeWithStringValue($stringValue) {
         $instance = new parent("tab-size", $stringValue);
         return $instance;
     }
-
+    
+    public static function stringValueLeft() {
+        return "left";
+    }
+    
+    public static function stringValueRight() {
+        return "right";
+    }
+    
+    public static function stringValueCenter() {
+        return "center";
+    }
+    
+    public static function stringValueJustify() {
+        return "justify";
+    }
+    
+    public static function DeclarationTextIndentWithBasicLength($basicLength) {
+        $instance = new parent("tab-size", $basicLength->stringBasicLength());
+        return $instance;
+    }
+    
+    public function stringDeclaration() {
+        $stringFullDeclaration = "";
+        $arrayStringBrowserPrefix = BrowserDiffer::getInstance()->getArrayStringBrowserPrefix();
+        foreach ($arrayStringBrowserPrefix as &$stringBrowserPrefix) {
+            $stringFullDeclaration = $stringFullDeclaration . $stringBrowserPrefix . $this->stringProperty . ":" . $this->stringValue . ";";
+        }
+        return $stringFullDeclaration;
+    }
     public static function stringValueNumber() {
         return "number";
     }
--- Website/4Carrus/View/CascadingStyleSheets/Common/Basic/DeclarationSpecifics/DeclarationTextAlignLast.php
+++ Website/4Carrus/View/CascadingStyleSheets/Common/Basic/DeclarationSpecifics/DeclarationTextAlignLast.php
@@ -51,6 +51,7 @@ class DeclarationTextAlignLast extends Declaration{
         }
         return $stringFullDeclaration;
     }
+    
 }
 
 ?>
