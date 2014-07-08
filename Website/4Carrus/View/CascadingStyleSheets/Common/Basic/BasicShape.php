1bd73ea212bfdb2fc08e0acc0dd33a3a5a55a281
Tarciso Torres <tarcisoftorres@gmail.com>
2014-07-08 16:41:17 -0300

    update

--- Website/4Carrus/View/CascadingStyleSheets/Common/Basic/BasicShape.php
+++ Website/4Carrus/View/CascadingStyleSheets/Common/Basic/BasicShape.php
@@ -1,11 +1,5 @@
 <?php
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /**
  * Description of Shape
  *
@@ -22,7 +16,7 @@ class BasicShape {
     }
 
     public static function BasicShapeWithStringShapeTypeWithStringShapeValue($stringShapeType, $stringShapeValue) {
-        $instance = new self($stringShapeType, $stringShapeValue); //arrumar
+        $instance = new self($stringShapeType, $stringShapeValue);
         return $instance;
     }
 
--- Website/4Carrus/View/CascadingStyleSheets/Common/Basic/BasicShapeSpecifics/BasicShapeRectangle.php
+++ Website/4Carrus/View/CascadingStyleSheets/Common/Basic/BasicShapeSpecifics/BasicShapeRectangle.php
@@ -1,16 +1,11 @@
 <?php
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /**
  * Description of Shape
  *
  * @author Judah
  */
+
 class BasicShapeRectangle {
 
     public static function BasicShapeRectangleWithBasicLengthTopWithBasicLengthRightWithBasicLengthBottomWithBasicLengthLeft($basicLengthTop, $basicLengthRight, $basicLengthBottom, $basicLengthLeft) {
