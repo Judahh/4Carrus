5637964526439093ed3b760795831a9a591d1aae
Tarciso Torres <tarcisoftorres@gmail.com>
2014-07-08 17:12:24 -0300

    update

---
+++ Website/4Carrus/View/CascadingStyleSheets/Common/Basic/BasicTranslateSpecifcs/BasicTranslateLongitudinal.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * BasicTranslateLongitudinal [TIPO]
+ * Descrição
+ * @copyright (c) year, Tarciso Torres Blue Lion
+ */
+
+class BasicTranslateLongitudinal extends BasicTranslate{
+    public static function BasicTranslateLongitudinalWithBasicLengthLongitudinal($basicLengthLongitudinal) {
+        $instance = new parent("z", $basicLengthLongitudinal->stringBasicLength());
+        return $instance;
+    }
+}
+
+?>
