09e43c353038bec19451e020446171cd673322fb
Judah Holanda Correia Lima <jh_esse@hotmail.com>
2014-07-08 16:47:30 -0300

    update

--- Website/4Carrus/View/CascadingStyleSheets/Common/Basic/BasicShapeSpecifics/BasicShapeRectangle.php
+++ Website/4Carrus/View/CascadingStyleSheets/Common/Basic/BasicShapeSpecifics/BasicShapeRectangle.php
@@ -5,12 +5,8 @@
  *
  * @author Judah
  */
-<<<<<<< HEAD
 class BasicShapeRectangle extends BasicShape{
-=======
 
-class BasicShapeRectangle {
->>>>>>> origin/master
 
     public static function BasicShapeRectangleWithBasicLengthTopWithBasicLengthRightWithBasicLengthBottomWithBasicLengthLeft($basicLengthTop, $basicLengthRight, $basicLengthBottom, $basicLengthLeft) {
         $instance = new parent("rect", $basicLengthTop->stringBasicLength() . "," . $basicLengthRight->stringBasicLength() . "," . $basicLengthBottom->stringBasicLength() . "," . $basicLengthLeft->stringBasicLength()); //arrumar
