f151883125cc977b65525bbd6ce2a2245c2192da
Tarciso Torres <tarcisoftorres@gmail.com>
2014-07-09 17:01:41 -0300

    update

---
+++ Website/4Carrus/View/CascadingStyleSheets/Common/Basic/DeclarationSpecifics/DeclarationTransformSpecifics/AuxiliaryTranslate.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * DeclarationTransformTranslate [TIPO]
+ * Descrição
+ * @copyright (c) year, Tarciso Torres Blue Lion
+ */
+class DeclarationTransformTranslate {
+    //put your code here
+}
+
+?>
