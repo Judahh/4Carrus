10b7adcf40c56fe5346425362b43fb140c31c25c
Tarciso Torres <tarcisoftorres@gmail.com>
2014-07-08 21:40:06 -0300

    update

--- Website/4Carrus/View/CascadingStyleSheets/Common/Basic/DeclarationSpecifics/DeclarationTransform.php
+++ Website/4Carrus/View/CascadingStyleSheets/Common/Basic/DeclarationSpecifics/DeclarationTransform.php
@@ -10,6 +10,38 @@ class DeclarationTransform extends Declaration{
         return $instance;
     }
     
+    public static function DeclarationTransformWithBasicMatrix($basicMatrix) {
+        $instance = new parent("transform", $basicMatrix->stringBasicMatrix());
+        return $instance;
+    }
+    
+    public static function DeclarationTransformWithBasicMatrixThreeDimencional($basicMatrixThreeDimencional) {
+        $instance = new parent("transform", $basicMatrixThreeDimencional->stringBasicMatrix());
+        return $instance;
+    }
+    
+    public static function DeclarationTransformWithBasicTranslate($basicTranslate) {
+        $instance = new parent("transform", $basicTranslate->stringBasicTranslate());
+        return $instance;
+    }
+    
+    public static function DeclarationTransformWithBasicTranslateThreeDimencional($basicTranslateThreeDimencional) {
+        $instance = new parent("transform", $asicTranslateThreeDimencional->stringBasicTranslate());
+        return $instance;
+    }
+    public static function DeclarationTransformWithBasicTranslateHorizontal($basicTranslateHorizontal) {
+        $instance = new parent("transform", $basicTranslateHorizontal->stringBasicTranslate());
+        return $instance;
+    }
+    public static function DeclarationTransformWithBasicTranslateVertical($basicTranslateVertical) {
+        $instance = new parent("transform", $basicTranslateVertical->stringBasicTranslate());
+        return $instance;
+    }
+    public static function DeclarationTransformWithBasicTranslate($basicTranslate) {
+        $instance = new parent("transform", $basicTranslate->stringBasicTranslate());
+        return $instance;
+    }
+    
     public function stringDeclaration() {
         $stringFullDeclaration = "";
         $arrayStringBrowserPrefix = BrowserDiffer::getInstance()->getArrayStringBrowserPrefix();
