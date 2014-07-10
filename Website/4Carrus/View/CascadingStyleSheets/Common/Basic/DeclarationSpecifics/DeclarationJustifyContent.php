e005d6cd63e5178667e0f6d8289e415e3e34f65d
silviomoreira <silviomoreira975@gmail.com>
2014-07-10 02:53:22 -0300

    update

--- Website/4Carrus/View/CascadingStyleSheets/Common/Basic/DeclarationSpecifics/DeclarationJustifyContent.php
+++ Website/4Carrus/View/CascadingStyleSheets/Common/Basic/DeclarationSpecifics/DeclarationJustifyContent.php
@@ -1,39 +1,42 @@
 <?php
 
-/**
- * DeclarationJustifyContent [TIPO]
- * Descrição
- * @copyright (c) year, Tarciso Torres Blue Lion
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
  */
 
-class DeclarationJustifyContent extends Declaration{
+/**
+ * Description of DeclarationJustifyContent
+ *
+ * @author Silvio
+ */
+class DeclarationJustifyContent extends Declaration {
 //Constructor
 //   instantiation: 
 //      $declaration = DeclarationJustifyContent::DeclarationJustifyContentStringValue($stringValue);
-        public static function DeclarationJustifyContentWithStringValue($stringValue) {
-            $instance = new parent("justify-content", $stringValue);
-            return $instance;
-        }
-        
-        public static function stringValueFlexStart() {
-            return "flex-start";
-        }
-        
-        public static function stringValueFlexEnd() {
-            return "flex-end";
-        }
-        
-        public static function stringValueCenter() {
-            return "center";
-        }
-        
-        public static function stringValueSpaceBetween() {
-            return "space-between";
-        }
-        
-        public static function stringValueSpaceAround() {
-            return "space-around";
-        }
-}
+    public static function DeclarationJustifyContentWithStringValue($stringValue) {
+        $instance = new parent("justify-content", $stringValue);
+        return $instance;
+    }
 
-?>
+    public static function stringValueFlexStart() {
+        return "flex-start";
+    }
+
+    public static function stringValueFlexEnd() {
+        return "flex-end";
+    }
+
+    public static function stringValueCenter() {
+        return "center";
+    }
+
+    public static function stringValueSpaceBetween() {
+        return "space-between";
+    }
+    
+    public static function stringValueSpaceAround() {
+        return "space-around";
+    }
+}
