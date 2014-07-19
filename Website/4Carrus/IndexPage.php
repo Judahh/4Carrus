37bb5acdde7b42b03ebe4fbaef9c5477c88f06ea
Tarciso Torres <tarcisoftorres@gmail.com>
2014-07-19 15:03:17 -0300

    update

--- Website/4Carrus/IndexPage.php
+++ Website/4Carrus/IndexPage.php
@@ -1,33 +1,35 @@
 <!DOCTYPE html>
 <html lang="pt-br">
-    <head>
-        <meta charset="UTF-8">
-        <title>4Carrus</title>
-        <link rel="stylesheet" type="text/css" href="View/CascadingStyleSheets/Common/Header/Header.css" />
-    </head>
-    <body>
-        <header>
-            <nav id="Menu">
-                <div id="BoxMenuSearchHolder"></div>
-                <div id="BoxMenuSearch">
-                    <ul>
-                        <li><div class="Icon">'</div></li>
-                    </ul>
-                </div>
+<head>
+    <meta charset="UTF-8">
+    <title>4Carrus</title>
+    <link rel="stylesheet" type="text/css" href="View/CascadingStyleSheets/Common/Header/Header.css" />
+</head>
+<body>
+<main>
+    <header>
+        <nav id="Menu">
+            <div id="BoxMenuSearchHolder"></div>
+            <div id="BoxMenuSearch">
+                <ul>
+                    <li><div class="Icon">'</div></li>
+                </ul>
+            </div>
 
-                <div id="BoxMenuOptionsHolder"></div>
-                <div id="BoxMenuOptions">
-                    <ul>
-                        <li><div class="Icon">›</div></li>
-                        <li><div class="Icon">¥</div></li>
-                        <li><div class="Icon">U</div></li>
-                        <li><div class="Icon">t</div></li>
-                    </ul>
-                </div>
-            </nav>
-        </header>
-        <?php
-        
-        ?>
-    </body>
+            <div id="BoxMenuOptionsHolder"></div>
+            <div id="BoxMenuOptions">
+                <ul>
+                    <li><div class="Icon">›</div></li>
+                    <li><div class="Icon">¥</div></li>
+                    <li><div class="Icon">U</div></li>
+                    <li><div class="Icon">t</div></li>
+                </ul>
+            </div>
+        </nav>
+    </header>
+    <?php
+    ?>
+</main>
+
+</body>
 </html>
