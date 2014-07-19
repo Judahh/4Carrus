ea5dcd167b239cff34650641832004318dd4e9dd
Tarciso Torres <tarcisoftorres@gmail.com>
2014-07-19 12:56:19 -0300

    update

--- Website/4Carrus/IndexPage.php
+++ Website/4Carrus/IndexPage.php
@@ -1,9 +1,4 @@
 <!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
 <html lang="pt-br">
     <head>
         <meta charset="UTF-8">
@@ -31,11 +26,8 @@ and open the template in the editor.
                 </div>
             </nav>
         </header>
-
-
         <?php
         
         ?>
     </body>
-
 </html>
