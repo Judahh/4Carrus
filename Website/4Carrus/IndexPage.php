703abbe8777c8d68a3ab2a3ac76b1b5e39b980d8
Tarciso Torres <tarcisoftorres@gmail.com>
2014-07-20 11:14:08 -0300

    update

--- Website/4Carrus/IndexPage.php
+++ Website/4Carrus/IndexPage.php
@@ -21,8 +21,22 @@
                 <ul>
                     <li><div class="Icon">›</div></li>
                     <li><div class="Icon">¥</div></li>
+                    <ul>
+                        <li><a href="#">Preferências</a></li>
+                        <li class="SubMenu"><a href="#">Cores</a></li>
+                        <li class="SubMenu"><a href="#">Idioma</a></li>
+                        <li class="SubMenu"><a href="#">Região</a></li>
+                    </ul>
                     <li><div class="Icon">U</div></li>
+                    <ul>
+                        <li class="SubMenu"><a href="#">Gas Station</a></li>
+                    </ul>
                     <li><div class="Icon">t</div></li>
+                    <ul>
+                        <li class="SubMenu"><a href="#">Minha Conta</a></li>
+                        <li class="SubMenu"><a href="#">Meu Carrinho</a></li>
+                        <li class="SubMenu"><a href="#">Minha Agenda</a></li>
+                    </ul>
                 </ul>
             </div>
         </nav>
