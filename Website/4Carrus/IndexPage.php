bb1a9471bf68012a2a434f8a00f92f570594f6a5
Tarciso Torres <tarcisoftorres@gmail.com>
2014-07-21 15:30:28 -0300

    update

--- Website/4Carrus/IndexPage.php
+++ Website/4Carrus/IndexPage.php
@@ -19,24 +19,24 @@
             <div id="BoxMenuOptionsHolder"></div>
             <div id="BoxMenuOptions">
                 <ul>
-                    <li><div class="Icon">›</div></li>
-                    <li><div class="Icon">¥</div></li>
-                    <ul>
-                        <li><a href="#">Preferências</a></li>
-                        <li class="SubMenu"><a href="#">Cores</a></li>
-                        <li class="SubMenu"><a href="#">Idioma</a></li>
-                        <li class="SubMenu"><a href="#">Região</a></li>
-                    </ul>
-                    <li><div class="Icon">U</div></li>
-                    <ul>
-                        <li class="SubMenu"><a href="#">Gas Station</a></li>
-                    </ul>
-                    <li><div class="Icon">t</div></li>
-                    <ul>
-                        <li class="SubMenu"><a href="#">Minha Conta</a></li>
-                        <li class="SubMenu"><a href="#">Meu Carrinho</a></li>
-                        <li class="SubMenu"><a href="#">Minha Agenda</a></li>
-                    </ul>
+                    <li class="Icon">›</li>
+                    <li class="Icon">¥</li>
+                        <ul>
+                            <li class="SubMenu"><a href="#">Preferências</a></li>
+                            <li class="SubMenu"><a href="#">Cores</a></li>
+                            <li class="SubMenu"><a href="#">Idioma</a></li>
+                            <li class="SubMenu"><a href="#">Região</a></li>
+                        </ul>
+                    <li class="Icon">U</li>
+                        <ul>
+                            <li class="SubMenu"><a href="#">Gas Station</a></li>
+                        </ul>
+                    <li class="Icon">t</li>
+                        <ul>
+                            <li class="SubMenu"><a href="#">Minha Conta</a></li>
+                            <li class="SubMenu"><a href="#">Meu Carrinho</a></li>
+                            <li class="SubMenu"><a href="#">Minha Agenda</a></li>
+                        </ul>
                 </ul>
             </div>
         </nav>
