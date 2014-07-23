f690a9133baab5c9790d6813c107b609d9a215bf
Tarciso Torres <tarcisoftorres@gmail.com>
2014-07-23 17:12:31 -0300

    update

--- Website/4Carrus/IndexPage.php
+++ Website/4Carrus/IndexPage.php
@@ -3,47 +3,50 @@
 <head>
     <meta charset="UTF-8">
     <title>4Carrus</title>
-    <link rel="stylesheet" type="text/css" href="View/CascadingStyleSheets/Common/Header/Header.css" />
+   <!-- <link rel="stylesheet" type="text/css" href="View/CascadingStyleSheets/Common/Header/Header.css" /> -->
+    <link href="Header.css" rel="stylesheet" type="text/css" media="all">
 </head>
 <body>
 <main>
     <header>
-        <div id="BoxMenuSearchHolder"></div>
-        <div id="BoxMenuSearch">
-            <ul>
-                <li><div class="Icon"><a href="#">'</a></div></li>
-            </ul>
+        <div id="BoxMenuTop">
+            <div id="BoxMenuSearchHolder"></div>
+            <div id="BoxMenuSearch">
+                <ul>
+                    <li class="Icon"><a href="#">'</a></li>
+                </ul>
+            </div>
+
+            <div id="BoxMenuOptionsHolder"></div>
+            <nav id="MenuOption">
+                <ul>
+                    <li><a href="#">›</a></li>
+                    <li><a href="#">¥</a>
+                        <ul>
+                            <li><a href="#">Preferências</a></li>
+                            <li><a href="#">Cores</a></li>
+                            <li><a href="#">Idioma</a></li>
+                            <li><a href="#">Região</a></li>
+                        </ul>
+                    </li>
+
+                    <li><a href="#">U</a>
+                        <ul>
+                            <li><a href="#">Gas Station</a></li>
+                        </ul>
+                    </li>
+
+                    <li><a href="#">t</a>
+                        <ul>
+                            <li><a href="#">Minha Conta</a></li>
+                            <li><a href="#">Meu Carrinho</a></li>
+                            <li><a href="#">Minha Agenda</a></li>
+                        </ul>
+                    </li>
+
+                </ul>
+            </nav>
         </div>
-
-        <div id="BoxMenuOptionsHolder"></div>
-        <nav id="Menu">
-            <ul>
-                <li class="Icon">›</li>
-                <li class="Icon">¥
-                <ul>
-                    <li class="SubMenu"><a href="#">Preferências</a></li>
-                    <li class="SubMenu"><a href="#">Cores</a></li>
-                    <li class="SubMenu"><a href="#">Idioma</a></li>
-                    <li class="SubMenu"><a href="#">Região</a></li>
-                </ul>
-                </li>
-
-                <li class="Icon">U
-                <ul>
-                    <li class="SubMenu"><a href="#">Gas Station</a></li>
-                </ul>
-                </li>
-
-                <li class="Icon">t
-                <ul>
-                    <li class="SubMenu"><a href="#">Minha Conta</a></li>
-                    <li class="SubMenu"><a href="#">Meu Carrinho</a></li>
-                    <li class="SubMenu"><a href="#">Minha Agenda</a></li>
-                </ul>
-                </li>
-
-            </ul>
-        </nav>
     </header>
     <?php
     ?>
