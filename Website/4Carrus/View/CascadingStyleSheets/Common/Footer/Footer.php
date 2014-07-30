bd348951b13398b1161d76f3a0a5b01cff6311da
Judah Holanda Correia Lima <jh_esse@hotmail.com>
2014-07-30 11:28:02 -0300

    FIX (PQP TARCISO, PARA DE FUDER O PROJETO)
    Para de entupir o projeto de teste!!! FOI ISSO QUE FUDEU O PROJETO
    TODO!!! Jah tinha falado isso 1000000x!!!!!

--- Website/4Carrus/View/CascadingStyleSheets/Common/Footer/Footer.php
+++ Website/4Carrus/View/CascadingStyleSheets/Common/Footer/Footer.php
@@ -1,7 +1,13 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Tarciso Torres
- * Date: 16/07/14
- * Time: 18:27
- */ 
+*{
+    margin: 0%;
+    padding: 0%;
+}
+
+footer #Information{
+    background: #008000 bottom;
+    position: fixed;
+    bottom: 0%;
+    left: 0%;
+    width: 20%;
+    color: #006699;
+}
