7c2f95b5fd8c9bc7a4300923edfdaa5388fa51e7
Tarciso Torres <tarcisoftorres@gmail.com>
2014-08-07 20:53:03 -0300

    update

--- Website/4Carrus/IndexPage.php
+++ Website/4Carrus/IndexPage.php
@@ -5,19 +5,19 @@
         <meta content="text/html" charset="UTF-8"/>
         
         <link href="View/CascadingStyleSheets/Search/Body/Common.css" rel="stylesheet" media="all">
-        
+
         <link href="View/CascadingStyleSheets/Common/Header/Common.css" rel="stylesheet" media="all">
         <link href="View/CascadingStyleSheets/Common/Footer/Common.css" rel="stylesheet" media="all">
         <link href="View/CascadingStyleSheets/Common/Common/Common.css" rel="stylesheet" media="all">
-        
+
         <link href="View/CascadingStyleSheets/Common/Body/Image/Common.css" rel="stylesheet" media="all">
         <link href="View/CascadingStyleSheets/Common/Body/Video/Common.css" rel="stylesheet" media="all">
-        
+
         <link href="View/CascadingStyleSheets/Common/Header/Menu/MenuOptions.css" rel="stylesheet" media="all">
         <link href="View/CascadingStyleSheets/Common/Header/Menu/MenuOptionsHolder.css" rel="stylesheet" media="all">
         <link href="View/CascadingStyleSheets/Common/Header/Menu/MenuSearch.css" rel="stylesheet" media="all">
         <link href="View/CascadingStyleSheets/Common/Header/Menu/MenuSearchHolder.css" rel="stylesheet" media="all">
-        
+
         <link href="View/CascadingStyleSheets/Common/Header/Menu/MenuSearch/SubMenuSearch.css" rel="stylesheet" media="all">
         <link href="View/CascadingStyleSheets/Common/Header/Menu/MenuSearch/SubMenuSearchHolder.css" rel="stylesheet" media="all">
         <link href="View/CascadingStyleSheets/Common/Header/Menu/MenuOptions/SubMenuNotifications.css" rel="stylesheet" media="all">
@@ -28,16 +28,16 @@
         <link href="View/CascadingStyleSheets/Common/Header/Menu/MenuOptions/SubMenuApplicationsHolder.css" rel="stylesheet" media="all">
         <link href="View/CascadingStyleSheets/Common/Header/Menu/MenuOptions/SubMenuUser.css" rel="stylesheet" media="all">
         <link href="View/CascadingStyleSheets/Common/Header/Menu/MenuOptions/SubMenuUserHolder.css" rel="stylesheet" media="all">
-        
+
         <link href="View/CascadingStyleSheets/Common/Footer/Menu/MenuVideo.css" rel="stylesheet" media="all">
         <link href="View/CascadingStyleSheets/Common/Footer/Menu/MenuVideoHolder.css" rel="stylesheet" media="all">
-        
+
         <link href="View/CascadingStyleSheets/Common/Footer/Menu/MenuVideo/SubMenuVideo.css" rel="stylesheet" media="all">
         <link href="View/CascadingStyleSheets/Common/Footer/Menu/MenuVideo/SubMenuVideoHolder.css" rel="stylesheet" media="all">
-        
+
         <link href="View/CascadingStyleSheets/Common/Footer/Menu/MenuInformation.css" rel="stylesheet" media="all">
         <link href="View/CascadingStyleSheets/Common/Footer/Menu/MenuInformationHolder.css" rel="stylesheet" media="all">
-        
+
         <link href="View/CascadingStyleSheets/Common/Footer/Menu/MenuInformation/SubMenuInformation.css" rel="stylesheet" media="all">
         <link href="View/CascadingStyleSheets/Common/Footer/Menu/MenuInformation/SubMenuInformationHolder.css" rel="stylesheet" media="all">
     </head>
