a43f505a592a96212b7dde842d54d825d8ea6fd9
Judah Holanda Correia Lima <judahholanda7@gmail.com>
2014-08-09 16:29:22 -0300

    update

--- Website/4Carrus/View/Frames/Common/Body/Search/Common.php
+++ Website/4Carrus/View/Frames/Common/Body/Search/Common.php
@@ -1,15 +1,3 @@
-<div id="DivIdBodyHolder">
-    <div id="DivIdDealership">
-        <img  id="ImgIdDealership" src="View/Carros/mercedessalzufer1.jpg"/>
-    </div>
-    <video id="VideoId">
-        <source src="View/Carros/Top Gear Music Video - The Most Beautiful Cars.mp4" type="video/mp4">
-    </video>
-    <div id="DivIdLogo">
-        <img  id="ImgIdLogo" src="View/Carros/4Carrus.jpg"/>
-    </div>
-</div>
-
 <main>
     <div id="DivIdTable">
         <?php
