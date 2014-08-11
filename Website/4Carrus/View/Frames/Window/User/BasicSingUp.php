a4e63819d538bdd4488f19c76ed1b4a60032c1d6
Tarciso Torres <tarcisoftorres@gmail.com>
2014-08-11 10:41:57 -0300

    update

--- Website/4Carrus/View/Frames/Window/User/BasicSingUp.php
+++ Website/4Carrus/View/Frames/Window/User/BasicSingUp.php
@@ -1,6 +1,9 @@
+<h1>Logo 4Carrus</h1>
+<h5>Cadastre-se em uma conta pessoal ou uma <a href="#">conta comercial</a></h5>
+<p id="Hint-Text">* Dados obrigatórios</p>
 <form>
     <ol>
-        <li>
+        <li id="LiIdFirstName">
             <label for="InputIdTextFirstName">
                 Nome:
                 <em>*</em>
@@ -8,7 +11,7 @@
             <input type="text" id="InputIdTextFirstName" size="25">
         </li>
 
-        <li>
+        <li id="LiIdSecondName">
             <label for="InputIdTextSecondName">
                 Sobrenome:
                 <em>*</em>
@@ -16,7 +19,7 @@
             <input type="text" id="InputIdTextSecondName" size="25">
         </li>
 
-        <li>
+        <li id="LiIdEmail">
             <label for="InputIdTextEmail">
                 E-mail:
                 <em>*</em>
@@ -24,15 +27,7 @@
             <input type="text" id="InputIdTextEmail" size="25">
         </li>
 
-        <li>
-            <label for="InputIdTextEmail">
-                E-mail:
-                <em>*</em>
-            </label>
-            <input type="text" id="InputIdTextEmail" size="25">
-        </li>
-
-        <li>
+        <li id="LiIdRepeatEmail">
             <label for="InputIdTextRepeatEmail">
                 Repetir e-mail:
                 <em>*</em>
@@ -40,7 +35,7 @@
             <input type="text" id="InputIdTextRepeatEmail" size="25">
         </li>
 
-        <li ng-repeat="phone in phones">
+        <li ng-repeat="phone in phones" id="LiIdPhone">
             <label for="InputTextPhone">
                 Telefone:
                 <em>*</em>
@@ -50,17 +45,27 @@
                 <option selected>Fixo</option>
                 <option>Celular</option>
                 <option>Comercial</option>
+                <option>Fax</option>
             </select>
             <input type="text" id="InputTextPhone" size="15">
             <input type="button" id="InputButtonPhone" ng-submit="addTodo() value="Add">
         </li>
 
-        <li>
-            <label for="InputIdTextRepeatEmail">
+        <li id="LiIdPassword">
+            <label for="InputIdPassword">
                 Definir senha:
                 <em>*</em>
             </label>
-            <input type="password" id="InputIdPasswordRepeatEmail" size="25">
+            <input type="password" id="InputIdPassword" size="25">
+            <span>Senha deve conter de 6 a 20 caracteres</span>
         </li>
     </ol>
+
+    <ul>
+        <li>Li e aceito o Contrato do Usuário e a Política de Privacidade.</li>
+        <li>Concordo em receber comunicações do 4Carrus e posso mudar minhas preferências de notificação em Meu 4Carrus.</li>
+        <li>Tenho, no mínimo, 18 anos de idade.</li>
+    </ul>
+
+    <input type="button" value="Enviar" id="InputIdButtonEnviar">
 </form>
