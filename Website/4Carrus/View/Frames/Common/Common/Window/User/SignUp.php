7a1d62f0ba15f81fd8164e215b592fc3ebb18dcf
Tarciso Torres <tarcisoftorres@gmail.com>
2014-08-12 11:13:06 -0300

    update

--- Website/4Carrus/View/Frames/Common/Common/Window/User/SignUp.php
+++ Website/4Carrus/View/Frames/Common/Common/Window/User/SignUp.php
@@ -1,15 +1,15 @@
 <form method="post" id="FormIdentificationUserSignUp" >
     <ol>
         <li id="LiIdNamePerson">
-            <label for="InputTextIDNamePerson">
+            <label for="InputTextIDNamePerson" id="LabelIdNamePerson">
                 Nome Completo:
                 <em>*</em>
             </label>
             <input type="text" id="InputTextIDNamePerson" size="45" maxlength="50" required="true">
         </li>
 
-        <li>
-            <label for="inputTextIdentificationUserCPF">
+        <li id="LiIdUserCPF">
+            <label for="inputTextIdentificationUserCPF" id="LabelIdUserCPF">
                 CPF:
                 <em>*</em>
             </label>
@@ -17,7 +17,7 @@
         </li>
 
         <li>
-            <label for="InputTextIDGeneralRegister">
+            <label for="InputTextIDGeneralRegister" id="LabelIdGeneralRegister">
                 RG:
                 <em>*</em>
             </label>
@@ -25,7 +25,7 @@
         </li>
 
         <li>
-            <label for="InputTextIDOrgãoEmissor">
+            <label for="InputTextIDOrgãoEmissor" id="LabelId">
                 Orgão Emissor
                 <em>*</em>
             </label>
@@ -33,15 +33,15 @@
         </li>
 
         <li>
-            <label for="InputDateIdExpedição">
+            <label for="InputDateIdExpedição" id="LabelId">
                 Data de Expedição
                 <em>*</em>
             </label>
-            <input type="date" id="InputDateIdExpedição">
+            <input type="date" id="InputDateIdExpedição" id="LabelId">
         </li>
 
         <li>
-            <label for="InputDateIdBirth">
+            <label for="InputDateIdBirth" id="LabelId">
                 Data Nascimento:
                 <em>*</em>
             </label>
@@ -49,13 +49,13 @@
         </li>
 
         <li>
-            <label >Sexo:</label>
-            <label for="InputRadioMan">M</label><input type="radio" name="Sexo" id="InputRadioMan" value="1" />
-            <label for="InputRadioWoman">F</label><input type="radio" name="Sexo" id="InputRadioWoman" value="2" />
+            <label id="LabelId">Sexo:</label>
+            <label for="InputRadioMan" id="LabelId>M</label><input type="radio" name="Sexo" id="InputRadioMan" value="1" />
+            <label for="InputRadioWoman" id="LabelId>F</label><input type="radio" name="Sexo" id="InputRadioWoman" value="2" />
         </li>
 
         <li>
-            <label for="SelectIDMaritalStatus">Estado Civil:</label>
+            <label for="SelectIDMaritalStatus" id="LabelId">Estado Civil:</label>
             <select id="SelectIDMaritalStatus">
                 <option selected>Solteiro(a)</option>
                 <option>Casado(a)</option>
@@ -63,49 +63,44 @@
                 <option>Viuvo(a)</option>
             </select>
         </li>
+
+        <li>
+            <label for="SelectIDPasture" id="LabelId">Logradouro</label>
+            <select id="SelectIDPasture">
+                <option selected>Rua</option>
+                <option>Avenida</option>
+                <option>Condominio</option>
+            </select>
+            <input type="text"  size="60" placeholder="ex: 537"/>
+        </li>
+
+        <li>
+            <label for="InputTextIdentificationNumber" id="LabelId">Número:</label>
+            <input type="text" id="InputTextIdentificationNumber" size="6"/>
+        </li>
+
+        <li>
+            <label for="InputTextIDNeighborhood" id="LabelId">Bairro:</label>
+            <input type="text" id="InputTextIDNeighborhood" size="40">
+        </li>
+
+        <li>
+            <label for="InputTextIdentificationPostalCode" id="LabelId">CEP:</label>
+            <input type="text" id="InputTextIdentificationPostalCode" size="9" placeholder="ex: 99999-999">
+        </li>
+
+        <li>
+            <label for="InputTextIDComplement" id="LabelId">Complemento:</label>
+            <input type="text" size="40" id="InputTextIDComplement" placeholder="ex: 2ª etapa">
+        </li>
     </ol>
-
-
-
         <tr>
-            <td><label for="SelectIDPasture">Logradouro</label></td>
-            <td>
-                <select id="SelectIDPasture">
-                    <option selected>Rua</option>
-                    <option>Avenida</option>
-                    <option>Condominio</option>
-                </select>
-                <input type="text"  size="60" placeholder="ex: 537"/>
-            </td>
-        </tr>
-
-        <tr>
-            <td><label for="InputTextIdentificationNumber">Número:</label></td>
-            <td><input type="text" id="InputTextIdentificationNumber" size="6"/></td>
-        </tr>
-
-        <tr>
-            <td><label for="InputTextIDNeighborhood">Bairro:</label></td>
-            <td><input type="text" id="InputTextIDNeighborhood" size="40"></td>
-        </tr>
-
-        <tr>
-            <td><label for="InputTextIdentificationPostalCode">CEP:</label></td>
-            <td><input type="text" id="InputTextIdentificationPostalCode" size="9" placeholder="ex: 99999-999"></td>
-        </tr>
-
-        <tr>
-            <td><label for="InputTextIDComplement">Complemento:</label></td>
-            <td><input type="text" size="40" id="InputTextIDComplement" placeholder="ex: 2ª etapa"></td>
-        </tr>
-
-        <tr>
-            <td><label for="InputTextIDCity">Municipio:</label></td>
+            <td><label for="InputTextIDCity" id="LabelId">Municipio:</label></td>
             <td><input type="text" id="InputTextIDCity" size="50" placeholder="ex: Fortaleza"><br/></td>
         </tr>
 
         <tr>
-            <td><label for="SelectIDState">Estado:</label></td>
+            <td><label for="SelectIDState" id="LabelId">Estado:</label></td>
             <td><select id="SelectIDState">
                 <option selected>CE</option>
                 <option>RJ</option>
@@ -114,8 +109,8 @@
         </tr>
 
         <tr>
-            <td><label for="SelectIDCountry">País:</label></td>
-            <td><select id="SelectIDCountry">
+            <label for="SelectIDCountry" id="LabelId">País:</label>
+            <select id="SelectIDCountry">
                 <option selected>United States of America</option>
                 <option>Brasil</option>
                 <option>China</option>
