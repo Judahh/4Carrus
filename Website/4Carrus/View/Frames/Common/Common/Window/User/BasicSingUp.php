25a31281f1277a2f7a8d40194b3c70a07eda0551
Tarciso Torres <tarcisoftorres@gmail.com>
2014-08-17 11:23:49 -0300

    update

--- Website/4Carrus/View/Frames/Common/Common/Window/User/BasicSingUp.php
+++ Website/4Carrus/View/Frames/Common/Common/Window/User/BasicSingUp.php
@@ -19,7 +19,31 @@
                             Nome:
                             <em>*</em>
                         </label>
-                        <input type="text" id="InputIdTextFirstName" size="25">
+                        <input type="text" id="InputIdTextFirstName" size="25"
+                               onclick="
+                                    this.style.background = '#F5F5F5',
+                                    this.style.borderColor = '#FFF'
+
+                                    if(this.value == 'Campo Obrigatório!'){
+                                        this.value = ''
+                                    }
+                               "
+
+                                onblur="
+                                    if(this.value == ''){
+                                        this.value = 'Campo Obrigatório!',
+                                        this.style.background = '#FFA6A6',
+                                        this.style.borderColor = 'red'
+                                    }
+                                "
+
+                                onchange="
+                                if(this.value.length > 1){
+                                  this.style.background = '#97FFB1',
+                                  this.style.borderColor = 'green'
+                                  }
+
+                                ">
                     </li>
 
                     <li id="LiIdSecondName">
@@ -27,7 +51,24 @@
                             Sobrenome:
                             <em>*</em>
                         </label>
-                        <input type="text" id="InputIdTextSecondName" size="25">
+                        <input type="text" id="InputIdTextSecondName" size="25"
+                               onclick="
+                                    this.style.background = '#F5F5F5',
+                                    this.style.borderColor = '#FFF'
+
+                                    if(this.value == 'Campo Obrigatório!'){
+                                        this.value = ''
+                                    }
+
+                               "
+
+                               onblur="
+                                    if(this.value == ''){
+                                        this.value = 'Campo Obrigatório!',
+                                        this.style.background = '#FFA6A6',
+                                        this.style.borderColor = 'red'
+                                    }
+                                ">
                     </li>
 
                     <li id="LiIdEmail">
@@ -35,7 +76,24 @@
                             E-mail:
                             <em>*</em>
                         </label>
-                        <input type="text" id="InputIdTextEmail" size="25">
+                        <input type="text" id="InputIdTextEmail" size="25"
+                               onclick="
+                                    this.style.background = '#F5F5F5',
+                                    this.style.borderColor = '#FFF'
+
+                                    if(this.value == 'Campo Obrigatório!'){
+                                        this.value = ''
+                                    }
+
+                               "
+
+                               onblur="
+                                    if(this.value == ''){
+                                        this.value = 'Campo Obrigatório!',
+                                        this.style.background = '#FFA6A6',
+                                        this.style.borderColor = 'red'
+                                    }
+                                ">
                     </li>
 
                     <li id="LiIdRepeatEmail">
@@ -43,7 +101,24 @@
                             Repetir e-mail:
                             <em>*</em>
                         </label>
-                        <input type="text" id="InputIdTextRepeatEmail" size="25">
+                        <input type="text" id="InputIdTextRepeatEmail" size="25"
+                               onclick="
+                                    this.style.background = '#F5F5F5',
+                                    this.style.borderColor = '#FFF'
+
+                                    if(this.value == 'Campo Obrigatório!'){
+                                        this.value = ''
+                                    }
+
+                               "
+
+                               onblur="
+                                    if(this.value == ''){
+                                        this.value = 'Campo Obrigatório!',
+                                        this.style.background = '#FFA6A6',
+                                        this.style.borderColor = 'red'
+                                    }
+                                ">
                     </li>
 
                     <li ng-repeat="phone in phones" id="LiIdPhone">
@@ -58,7 +133,24 @@
                             <option>Comercial</option>
                             <option>Fax</option>
                         </select>
-                        <input type="text" id="InputTextPhone" size="15">
+                        <input type="text" id="InputTextPhone" size="15"
+                               onclick="
+                                    this.style.background = '#F5F5F5',
+                                    this.style.borderColor = '#FFF'
+
+                                    if(this.value == 'Campo Obrigatório!'){
+                                        this.value = ''
+                                    }
+
+                               "
+
+                               onblur="
+                                    if(this.value == ''){
+                                        this.value = 'Campo Obrigatório!',
+                                        this.style.background = '#FFA6A6',
+                                        this.style.borderColor = 'red'
+                                    }
+                                ">
                         <button type="button"id="InputButtonPhone" ng-submit="addTodo()" >Add</button>
                     </li>
 
@@ -67,7 +159,28 @@
                             Definir senha:
                             <em>*</em>
                         </label>
-                        <input type="password" id="InputIdPassword" size="25">
+                        <input type="password" id="InputIdPassword" size="25"
+                               onclick="
+                                    this.style.background = '#F5F5F5',
+                                    this.style.borderColor = '#FFF'
+
+                                    if(this.value == 'Campo Obrigatório!'){
+                                        this.value = ''
+                                    }
+
+                               "
+
+                               onblur="
+                                    if(this.value == ''){
+                                        this.value = 'Campo Obrigatório!',
+                                        this.style.background = '#FFA6A6',
+                                        this.style.borderColor = 'red'
+                                        this.input.type =
+                                    }
+                                "
+
+
+                                ">
                         <span>Senha deve conter de 6 a 20 caracteres</span>
                     </li>
                 </ol>
@@ -77,7 +190,10 @@
                         <input type="checkbox" id="InputCheckBox">
                         <label for="InputCheckBox" id="Label">Li e aceito o <a href="#">Contrato do Usuário</a> e a <a href="#">Política de Privacidade.</a></label>
                     </li>
-                    <li>Concordo em receber comunicações do 4Carrus e posso mudar minhas preferências de notificação em Meu 4Carrus.</li>
+                    <li>
+                        <input type="checkbox" id="InputCheckBoxNewsLetter">
+                        <label for="InputCheckBoxNewsLetter">Concordo em receber comunicações do 4Carrus e posso mudar minhas preferências de notificação em Meu 4Carrus.</label>
+                    </li>
                 </ul>
 
                 <input type="button" value="Enviar" id="InputIdButtonEnviar">
@@ -85,6 +201,5 @@
         </form>
     </div>
 </div>
-
 
 </html>
