dbdc3c5c58c490de3215f0c22938fb6410e5f197
Tarciso Torres <tarcisoftorres@gmail.com>
2014-08-21 21:02:52 -0300

    update

---
+++ Website/4Carrus/View/Frames/Common/Common/Window/Dealership/SingUpCar.php
@@ -0,0 +1,81 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Tarciso Torres
+ * Date: 21/08/14
+ * Time: 20:50
+ */
+?>
+<form id="" name="" method="post" action="">
+    <ol>
+        <li>
+            <label for="InputTextModel">Modelo:</label>
+            <input type="text" id="InputTextModel" name="InputNameModel">
+        </li>
+
+        <li>
+            <label for="InputTextBrand">Marca:</label>
+            <input type="text" id="InputTextBrand" name="InputNameBrand">
+        </li>
+
+        <li>
+            <label for=""></label>
+            <input type="text" id="InputText" name="InputName">
+        </li>
+
+        <li>
+            <label for=""></label>
+            <input type="text" id="InputText" name="InputName">
+        </li>
+
+        <li>
+            <label for=""></label>
+            <input type="text" id="InputText" name="InputName">
+        </li>
+
+        <li>
+            <label for=""></label>
+            <input type="text" id="InputText" name="InputName">
+        </li>
+
+        <li>
+            <label for=""></label>
+            <input type="text" id="InputText" name="InputName">
+        </li>
+
+        <li>
+            <label for=""></label>
+            <input type="text" id="InputText" name="InputName">
+        </li>
+
+        <li>
+            <label for=""></label>
+            <input type="text" id="InputText" name="InputName">
+        </li>
+
+        <li>
+            <label for=""></label>
+            <input type="text" id="InputText" name="InputName">
+        </li>
+
+        <li>
+            <label for=""></label>
+            <input type="text" id="InputText" name="InputName">
+        </li>
+
+        <li>
+            <label for=""></label>
+            <input type="text" id="InputText" name="InputName">
+        </li>
+
+        <li>
+            <label for=""></label>
+            <input type="text" id="InputText" name="InputName">
+        </li>
+
+        <li>
+            <label for=""></label>
+            <input type="text" id="InputText" name="InputName">
+        </li>
+    </ol>
+</form>
