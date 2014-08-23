03145121ff31b69e20c1db68ee20db61991a5320
Tarciso Torres <tarcisoftorres@gmail.com>
2014-08-23 09:55:47 -0300

    update

--- Website/4Carrus/View/Frames/Common/Common/Window/Dealership/SingUpCar.php
+++ Website/4Carrus/View/Frames/Common/Common/Window/Dealership/SingUpCar.php
@@ -9,73 +9,74 @@
 <form id="" name="" method="post" action="">
     <ol>
         <li>
+            <label for="InputTextAutoMaker">Marca:</label>
+            <input type="text" id="InputTextAutoMaker" name="InputNameMaker" placeholder="Chevrolet">
+        </li>
+
+        <li>
             <label for="InputTextModel">Modelo:</label>
-            <input type="text" id="InputTextModel" name="InputNameModel">
+            <input type="text" id="InputTextModel" name="InputNameModel" placeholder="Celta">
         </li>
 
         <li>
-            <label for="InputTextBrand">Marca:</label>
-            <input type="text" id="InputTextBrand" name="InputNameBrand">
+            <label for="InputTextSubModel">Sub. Modelo:</label>
+            <input type="text" id="InputTextSubModel" name="InputNameSubModel" placeholder="LT">
+        </li>
+
+
+        <li>
+            <label for="InputTextYear">Ano:</label>
+            <input type="text" id="InputTextYear" name="InputNameYear" placeholder="2011">
         </li>
 
         <li>
-            <label for=""></label>
-            <input type="text" id="InputText" name="InputName">
+            <label for="">Direção:</label>
+            <input type="text" id="InputText" name="InputName" placeholder="Hidráulica">
         </li>
 
         <li>
-            <label for=""></label>
-            <input type="text" id="InputText" name="InputName">
+            <label for="InputTextTransmission">Cambio:</label>
+            <input type="text" id="InputTextTransmission" name="InputNameTransmission" placeholder="Manual">
         </li>
 
         <li>
-            <label for=""></label>
-            <input type="text" id="InputText" name="InputName">
+            <label for="InputTextEngine">Motor:</label>
+            <input type="text" id="InputTextEngine" name="InputNameEngine" placeholder="vhc">
         </li>
 
         <li>
-            <label for=""></label>
-            <input type="text" id="InputText" name="InputName">
+            <label for="InputTextNumberOfCylinders">Número de Cilindros:</label>
+            <input type="text" id="InputTextNumberOfCylinders" name="InputNameNumberOfCylinders" placeholder="4">
         </li>
 
         <li>
-            <label for=""></label>
-            <input type="text" id="InputText" name="InputName">
+            <label for="InputTextDriveType">Tração:</label>
+            <input type="text" id="InputTextDriveType" name="InputNameDriveType" placeholder="Dianteira">
         </li>
 
         <li>
-            <label for=""></label>
-            <input type="text" id="InputText" name="InputName">
+            <label for="InputTextExteriorColor">Cor Exterior:</label>
+            <input type="text" id="InputTextExteriorColor" name="InputNameExteriorColor">
         </li>
 
         <li>
-            <label for=""></label>
-            <input type="text" id="InputText" name="InputName">
+            <label for="InputTextInteriorColor">Cor Interior:</label>
+            <input type="text" id="InputTextInteriorColor" name="InputNameInteriorColor">
         </li>
 
         <li>
-            <label for=""></label>
-            <input type="text" id="InputText" name="InputName">
+            <label for="InputTextBodyType">Tipo Corpo:</label>
+            <input type="text" id="InputTextBodyType" name="InputNameBodyType" placeholder="Sedan">
         </li>
 
         <li>
-            <label for=""></label>
-            <input type="text" id="InputText" name="InputName">
+            <label for="InputTextFuelType">Combustivel:</label>
+            <input type="text" id="InputTextFuelType" name="InputNameFuelType" placeholder="Flex">
         </li>
 
         <li>
-            <label for=""></label>
-            <input type="text" id="InputText" name="InputName">
-        </li>
-
-        <li>
-            <label for=""></label>
-            <input type="text" id="InputText" name="InputName">
-        </li>
-
-        <li>
-            <label for=""></label>
-            <input type="text" id="InputText" name="InputName">
+            <label for="InputTextNumberOfDoors">Número de Portas:</label>
+            <input type="text" id="InputTextNumberOfDoors" name="InputNameNumberOfDoors" placeholder="4">
         </li>
     </ol>
 </form>
