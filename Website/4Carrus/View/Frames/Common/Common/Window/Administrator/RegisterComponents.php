8b1a8b3631e911d40aa39af96760244e01c39b93
Tarciso Torres <tarcisoftorres@gmail.com>
2014-08-27 22:16:52 -0300

    update

---
+++ Website/4Carrus/View/Frames/Common/Common/Window/Administrator/RegisterComponents.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Tarciso Torres
+ * Date: 25/08/14
+ * Time: 11:13
+ */
+?>
+<form id="FormIdRegisterComponents" name="FormNameRegisterComponents" action="" method="get">
+    <ol>
+        <li>
+            <label for="InputIdColor">Cor:</label>
+            <input type="text" id="InputIdColor" name="InputNameColor" placeholder="Insira o nome de uma cor">
+        </li>
+
+        <li>
+            <label for="InputIdTypeColor">Tipo Cor:</label>
+            <input type="text" id="InputIdTypeColor" name="InputNameTypeColor" placeholder="Insira o nome de uma cor">
+        </li>
+
+        <li>
+            <label for="">Modelo Motor</label>
+            <input type="text" id="InputIdMotorModel">
+        </li>
+
+    </ol>
+</form>
--- Website/4Carrus/View/Frames/Common/Common/Window/Administrator/SingUpComponents.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Tarciso Torres
- * Date: 25/08/14
- * Time: 11:13
- */ 
