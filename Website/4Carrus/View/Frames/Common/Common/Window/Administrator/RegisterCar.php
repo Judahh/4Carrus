f89531626628a3326367dc71393f0887412685aa
Tarciso Torres <tarcisoftorres@gmail.com>
2014-09-02 23:44:33 -0300

    update

---
+++ Website/4Carrus/View/Frames/Common/Common/Window/Administrator/RegisterCar.php
@@ -0,0 +1,76 @@
+<form id="" name="" method="post" action="">
+    <ol>
+        <li>
+            <label for="InputTextAutoMaker">Marca:</label>
+            <input type="text" id="InputTextAutoMaker" name="InputNameMaker" placeholder="Chevrolet">
+        </li>
+
+        <li>
+            <label for="InputTextModel">Modelo:</label>
+            <input type="text" id="InputTextModel" name="InputNameModel" placeholder="Celta">
+        </li>
+
+        <li>
+            <label for="InputTextSubModel">Sub. Modelo:</label>
+            <input type="text" id="InputTextSubModel" name="InputNameSubModel" placeholder="LT">
+        </li>
+
+
+        <li>
+            <label for="InputTextYear">Ano:</label>
+            <input type="text" id="InputTextYear" name="InputNameYear" placeholder="2011">
+        </li>
+
+        <li>
+            <label for="">Direção:</label>
+            <input type="text" id="InputText" name="InputName" placeholder="Hidráulica">
+        </li>
+
+        <li>
+            <label for="InputTextTransmission">Cambio:</label>
+            <input type="text" id="InputTextTransmission" name="InputNameTransmission" placeholder="Manual">
+        </li>
+
+        <li>
+            <label for="InputTextEngine">Motor:</label>
+            <input type="text" id="InputTextEngine" name="InputNameEngine" placeholder="vhc">
+        </li>
+
+        <li>
+            <label for="InputTextNumberOfCylinders">Número de Cilindros:</label>
+            <input type="text" id="InputTextNumberOfCylinders" name="InputNameNumberOfCylinders" placeholder="4">
+        </li>
+
+        <li>
+            <label for="InputTextDriveType">Tração:</label>
+            <input type="text" id="InputTextDriveType" name="InputNameDriveType" placeholder="Dianteira">
+        </li>
+
+        <li>
+            <label for="InputTextExteriorColor">Cor Exterior:</label>
+            <input type="text" id="InputTextExteriorColor" name="InputNameExteriorColor">
+        </li>
+
+        <li>
+            <label for="InputTextBodyType">Tipo Corpo:</label>
+            <input type="text" id="InputTextBodyType" name="InputNameBodyType" placeholder="Sedan">
+        </li>
+
+        <li>
+            <label for="InputTextFuelType">Combustivel:</label>
+            <input type="text" id="InputTextFuelType" name="InputNameFuelType" placeholder="Flex" list="DatalistIdFuelType">
+            <datalist id="DatalistIdFuelType">
+                <option>Gasolina</option>
+                <option>Álcool</option>
+                <option>Flex</option>
+                <option>Disel</option>
+                <option>Gás Natural</option>
+
+            </datalist>
+        </li>
+
+        <li>
+            <label for="InputTextNumberOfDoors">Número de Portas:</label>
+            <input type="text" id="InputTextNumberOfDoors" name="InputNameNumberOfDoors" placeholder="4">
+        </li>
+    </ol>
--- Website/4Carrus/View/Frames/Common/Common/Window/Administrator/RegisterComponents.php
+++ Website/4Carrus/View/Frames/Common/Common/Window/Administrator/RegisterComponents.php
@@ -15,12 +15,22 @@
 
         <li>
             <label for="InputIdTypeColor">Tipo Cor:</label>
-            <input type="text" id="InputIdTypeColor" name="InputNameTypeColor" placeholder="Insira o nome de uma cor">
+            <input type="text" id="InputIdTypeColor" name="InputNameTypeColor" placeholder="Metalica">
         </li>
 
         <li>
-            <label for="">Modelo Motor</label>
-            <input type="text" id="InputIdMotorModel">
+            <label for="InputIdMotorMotorModel">Modelo Motor:</label>
+            <input type="text" id="InputIdMotorMotorModel" name="InputNameMotorModel" placeholder="ex: VHC-E">
+        </li>
+
+        <li>
+            <label for="InputIdAutoMaker">Marca:</label>
+            <input type="text" id="InputIdAutoMaker" name="InputNameInputIdAutoMaker" placeholder="ex: Chevrolet">
+        </li>
+
+        <li>
+            <label for="InputIdCarModel">Modelo:</label>
+            <input type="text" id="InputIdCarModel" name="InputNameCarModel" placeholder="ex: Celta">
         </li>
 
     </ol>
